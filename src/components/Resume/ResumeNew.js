b50acc11a012f5314454b46ee4f07ce6ef1d7cbb
biniyam-fekede <141510983+biniyam-fekede@users.noreply.github.com>
2024-10-27 00:06:43 -0700

    changes

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -5,6 +5,7 @@ import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 import pdf from "../../Assets/Resume_2025.pdf";
+import pdf from "../../Assets/MyResume_Amazon.pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 function ResumeNew() {
