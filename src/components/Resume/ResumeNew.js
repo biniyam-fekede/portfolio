b0d7c4a34cda7174c77943a66948f616f2bc07c3
biniyam <biniyam@uw.edu>
2024-12-25 21:49:32 -0800

    Resolved merge conflicts

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -4,7 +4,7 @@ import Button from "react-bootstrap/Button";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
-import pdf from "../../Assets/Resume_2025.pdf";
+//import pdf from "../../Assets/Resume_2025.pdf";
 import pdf from "../../Assets/MyResume_Amazon.pdf";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
