cd0151e13a1980a045cd3e3bb52d99b0daa8b6c5
biniyam <biniyam@uw.edu>
2024-12-25 21:31:54 -0800

    Demo

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -38,7 +38,7 @@ function Projects() {
                 isBlog={false}
                 title="Stuck in the Mud"
                 description="Stuck in the Mud is a Java-based dice game using a circular linked list and custom iterators. Players roll dice, and any 2s or 5s get “stuck,” affecting the turn. The game ends when a player reaches 100 points. This project showcases linked list operations, custom iterators, and basic game logic."
-                // demoLink="https://bloggery-liu8.vercel.app/"
+                demoLink="https://youtu.be/22usGVhaMZs"
                 ghLink="https://github.com/biniyam-fekede/StuckInTheMud-Game"
               />
             </Col>
@@ -47,10 +47,10 @@ function Projects() {
               <ProjectCard
                 imgPath={globalWeather}
                 isBlog={false}
-                title="The Global Weather"
+                title="Global Weather Manager"
                 description="The Global Weather project is a Java-based application that processes a large weather dataset using ArrayList, Binary Search, and Linear Regression. It involves creating a GlobalWeatherManager class to manage the data and implement sorting, searching, and statistical analysis for various cities. The project aims to enhance skills in data structures, file handling, and algorithm optimization."
+                demoLink="https://youtu.be/skoZaBpgKtg"
                 ghLink="https://github.com/biniyam-fekede/Global-Weather-Manager"
-                // demoLink="https://netflix-clone-taupe-eight.vercel.app/"
               />
             </Col>
 
@@ -60,8 +60,8 @@ function Projects() {
                 isBlog={false}
                 title="The Book Browsing"
                 description="The Book Browsing by Selected Field project is a Java application that allows users to browse a collection of over 9,000 books using a graphical interface. The project uses data structures like Binary Search Trees (BST) and TreeMap to sort and display books by fields such as ISBN, title, or average rating. The GUI includes navigation buttons for easy browsing, allowing users to move through the book collection and filter by selected fields."
-                // ghLink="https://github.com/fitiha/Amazon-clone.git"
-                // demoLink="https://amazon-clone-hazel-eight.vercel.app/"
+                demoLink="https://youtu.be/fxn8qjG3zgI"
+                ghLink="https://github.com/biniyam-fekede/Book-Browsing-App"
               />
             </Col>
 
@@ -71,8 +71,8 @@ function Projects() {
                 isBlog={false}
                 title="My Portfolio"
                 description="I created a simple yet modern portfolio to showcase my projects and skills. Built with React, CSS, and Bootstrap, the design features a clean layout and intuitive navigation, allowing visitors to explore my work effortlessly. The aesthetic blends vibrant visuals with a user-friendly interface, highlighting my passion for both design and functionality. This project not only reflects my technical abilities but also my commitment to delivering a seamless user experience. Explore my portfolio to see my journey and creativity in action!"
-                // ghLink="https://github.com/fitiha/TodolistApp.git"
-                // demoLink="https://adeysecrets.onrender.com"
+                demoLink="https://biniyamgebreyohannes.vercel.app/project"
+                ghLink="https://github.com/biniyam-fekede/portfolio"
               />
             </Col>
 
